6f3558a51a1b0d5ebe58e1218da6a6ac276db63e
User <User@WIN-D6I7BGDFHRA>
2022-12-11 19:33:52 +0100

    Добавьте файлы проекта.

---
+++ HomeWork 31.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdio.h>
+
+
+int main()
+{
+    FILE* file;
+    fopen_s(&file, "D:/C++_gamProject/HomeWork 31/hw.txt", "a+");
+
+
+    char* word = new char[25];
+    std::cout << "Word:";
+    std::gets(word);
+    fseek(file, 0, SEEK_SET);
+
+
+   while (true)
+    {
+        if ((char*)fgetc(file) == " ")
+        {
+            fseek(file, -1, SEEK_CUR);
+            if (word[-1] == (char)fgetc(file))
+            {
+                fseek(file, -1, SEEK_CUR);
+                if (word[-2] == (char)fgetc(file))
+                {
+                    for (int i = -10; i <= 2; i++)
+                    {
+                        fseek(file, i, SEEK_CUR);
+                        char s = fgetc(file);
+                        std::cout << s;
+                    }
+                    fputs("\n", file);
+                    std::cout << "\n";
+                }
+            }
+        }
+        else
+            fseek(file, 1, SEEK_CUR);
+
+
+        if (feof(file) == 0)
+            continue;
+        else
+            break;
+    }
+    delete[] word;
+}
